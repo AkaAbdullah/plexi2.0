f5c0955719a7eac5215766ca9ddbe96c65bf6573
Abdullah Hussain <akaabdullah00@gmail.com>
2023-12-29 19:51:30 +0500

    tutorial added

---
+++ frontend/src/assets/images.jsx
@@ -0,0 +1,17 @@
+const IMAGES = {
+  img1: new URL("../assets/1.PNG", import.meta.url).href,
+  img2: new URL("../assets/2.PNG", import.meta.url).href,
+  img3: new URL("../assets/3.PNG", import.meta.url).href,
+  img4: new URL("../assets/4.PNG", import.meta.url).href,
+  img5: new URL("../assets/5.png", import.meta.url).href,
+  img6: new URL("../assets/6.PNG", import.meta.url).href,
+  img7: new URL("../assets/7.jpeg", import.meta.url).href,
+  img8: new URL("../assets/8.jpeg", import.meta.url).href,
+  img9: new URL("../assets/9.PNG", import.meta.url).href,
+  img10: new URL("../assets/10.PNG", import.meta.url).href,
+  img11: new URL("../assets/11.PNG", import.meta.url).href,
+  img12: new URL("../assets/12.PNG", import.meta.url).href,
+  img13: new URL("../assets/13.PNG", import.meta.url).href,
+  img14: new URL("../assets/14.PNG", import.meta.url).href,
+};
+export default IMAGES;
